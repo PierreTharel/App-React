d373476888dfeeef52fb9a8e44a2a4de0c074b9f
Pierre-Olivier Tharel <pierre.tharel@gmail.com>
2025-01-06 11:13:24 +0100

    first commit of App-React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './Hello.jsx'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Home />
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+const Hello = () => {
+
+    return(
+        <h1>Hello React World </h1>
+    )
+}
+
+export default Hello
