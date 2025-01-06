0be7bb914add9224b3066b2e3b4d9c45456875a9
Pierre-Olivier Tharel <pierre.tharel@gmail.com>
2025-01-06 12:26:39 +0100

    ajout Avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-import Home from './Hello.jsx'
+import Hello from './Hello.jsx'
+import Avatar from './Avatar.jsx'
 
 function App() {
+
+  const avatar = [
+    {
+      firstName : "Homer",
+      lastname : "Simpson",
+      image : "https://www.webstickersmuraux.com/fr/img/as045mb-png/folder/products-detalle-png/autocollants-homer-simpson.png"
+    },
+    {
+      firstName : "Bart",
+      lastname : "Simpson",
+      image : "https://thumbs.dreamstime.com/b/bart-simpson-dessin-anim%C3%A9-vektor-formats-jpg-png-262242958.jpg"
+    }
+  ]
 
   return (
     <>
     <div>
-      <Home />
+      <Hello />
+      <Avatar />
+
     </div>
 
     </>
---
+++ src/Avatar.jsx
@@ -0,0 +1,11 @@
+function Avatar(props) {
+    return (
+        <div>
+        {avatar.firstname} 
+        {avatar.lastname} 
+        {avatar.image}
+        </div>
+    );
+  }
+    
+export default Avatar
