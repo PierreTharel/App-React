ed215c066983698fc6ec77349244fcfd66b7561f
Pierre-Olivier Tharel <pierre.tharel@gmail.com>
2025-01-07 12:21:04 +0100

    modif Context

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,38 +1,49 @@
 import { useEffect, useState } from "react"
-import Users from "./User"
+import axios from 'axios'
+
+
 
 const Weather = () => {
 
-    const geo = useState(Users)
+    const [city, setCity] = useState(city)
+    const [result, setResult] = useState(null)
+    const [search, setSearch] = useState(city)
     const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
-const fetchWeather = async () => {
-    try{
-        const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=${geo}&appid=${API_KEY}&units=metric')
-    } 
-    catch (err) {
-        console.log (err)
+    const fetchWeather = async () => {
+        setCity(search)
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${geo}&appid=${API_KEY}&units=metric`)
+            setResult(response.data)
+        }
+        catch (err) {
+            console.error(err)
+        }
     }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [])
+
+    console.log(result)
+
+    return (
+        <>
+
+
+            <div className="flex flex-col justify-center items-center " >
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    fetchWeather()
+                }}>
+                    <input type="text" onChange={e => setSearch(e.target.value)} />
+                    <input type="submit" value='search' />
+                </form>
+                <h1>Weather in {city} : {result && result.main.temp} degrees </h1>
+            </div>
+        </>
+    )
 }
 
-useEffect(() => {
-    fetchWeather()
-}, [])
-
-return (
-    <>
-        <div className="flex flex-col justify-center items-center " >
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                fetchWeather()
-            }}>
-                <input type="text" onChange={e => setSearch(e.target.value)} />
-                <input type="submit" value='search' />
-            </form>
-            <h1>Weather in {geo} : {result && result.main.temp} degrees </h1>
-        </div>
-    </>
-)
-}
 
 export default Weather
